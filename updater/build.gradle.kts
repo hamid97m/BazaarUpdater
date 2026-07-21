a2f90eb0dce419ee9d815f7a3c0644342d7808dc
Hamid Mahmoodi <shr.mahmoodi@gmail.com>
2026-07-21 17:56:20 +0330

    New version 1.1.2

--- updater/build.gradle.kts
+++ updater/build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
         create("release", MavenPublication::class) {
             groupId = "com.farsitel.bazaar"
             artifactId = "updater"
-            version = "1.1.1-alpha1"
+            version = "1.1.2"
 
             afterEvaluate {
                 from(components["release"])
